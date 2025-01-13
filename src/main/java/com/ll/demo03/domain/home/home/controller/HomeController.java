136295bb542f7cefcd57f4f620d24716b20178a5
yoon2000 <55117130+yoon2000@users.noreply.github.com>
2025-01-13 17:15:39 +0900

    test

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hello, World!!!!!, on " + siteName;
+        return "Hello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!, on " + siteName;
     }
 
     @GetMapping("/secretKey")
